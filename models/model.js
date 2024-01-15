dba3767cce5319af70a446f689cc688d6c0fd000
Arranu <60885158+Arranu@users.noreply.github.com>
2024-01-15 19:41:28 +0000

    challenge 4 complete - with working errors

--- models/model.js
+++ models/model.js
@@ -7,3 +7,10 @@ exports.topicsMod = () => {
     })
 }
 
+exports.specArtMod = (iD)=>{
+    return db.query(`SELECT * FROM articles WHERE article_id = $1;`, [iD]).then(({rows})=>{
+        if(rows.length < 1) return Promise.reject({status:404 ,msg:'article does not exist'})
+        return rows.shift()
+    })
+}
+
