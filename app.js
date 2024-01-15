f32a9ad15b2c8f7138073f7eae9ff8c73fbdbde3
Arranu <60885158+Arranu@users.noreply.github.com>
2024-01-15 15:03:48 +0000

    challenge 3 complete

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express")
 const app = express()
-const {topicsCont} = require("./controllers/controller")
+const {topicsCont, apiCont} = require("./controllers/controller")
 const {notFoundInput} = require("./error-handling/error-handlers")
 //get all topics
 app.get("/api/topics", topicsCont)
+
+//get all endpoints
+app.get("/api", apiCont)
 
 app.all("/*", notFoundInput)
 module.exports = app
--- controllers/controller.js
+++ controllers/controller.js
@@ -1,7 +1,12 @@
 const {topicsMod} = require("../models/model")
+const apiFile = require("../endpoints.json")
 exports.topicsCont = (req,res,next)=>{    
 topicsMod().then((result)=>{
     res.status(200).send({topics:result})
     
 })
+}
+exports.apiCont = (req,res)=>{
+
+    res.status(200).send({endpoints:apiFile})
 }
